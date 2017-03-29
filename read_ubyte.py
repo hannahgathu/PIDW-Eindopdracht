20557a62db0ab924a197ff27931bf4d7194f06e5
raymondedwards95 <raymondedwards95@gmail.com>
2017-03-29 14:45:21 +0200

    Eerste versie read_ubyte.py

--- read_ubyte.py
+++ read_ubyte.py
@@ -3,5 +3,20 @@ import autograd.numpy as np
 file_labels = open('t10k-labels.idx1-ubyte', 'rb')
 file_images = open('t10k-images.idx3-ubyte', 'rb')
 
-file_labels.seek(9)
-print(value)
+file_labels.seek(8)
+Y = np.fromfile(file_labels, dtype=np.byte)
+
+# file_images.seek(0)
+# magic = np.fromfile(file_images, dtype=np.int32, count=1)
+# num_img = np.fromfile(file_images, dtype=np.int32, count=1)
+# num_row = np.fromfile(file_images, dtype=np.int32, count=1)
+# num_col = np.fromfile(file_images, dtype=np.int32, count=1)
+
+# M = np.fromfile(file_images, dtype=np.byte, count=5)
+file_images.seek(16)
+pixels = np.fromfile(file_images, dtype=np.byte)
+X = np.reshape(pixels, [10000, -1])
+print(X.shape)
+
+file_labels.close()
+file_images.close()
