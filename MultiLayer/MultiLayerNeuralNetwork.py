4f0b28ab3503ae23a21275995a3434bab68c71c9
MarikenWeijs <mariken.weijs@gmail.com>
2017-03-30 12:51:13 +0200

    Beginnetje multilayer

--- MultiLayer/MultiLayerNeuralNetwork.py
+++ MultiLayer/MultiLayerNeuralNetwork.py
@@ -3,7 +3,7 @@ from autograd import grad
 
 class MultiLayerNeuralNetwork():
     """ Neuraal netwerk met één verborgen laag:
-        k is het aantal eenheden in de verborgen laag
+        k is een lijst van de aantallen eenheden per verborgen laag
         X is de invoer van data om te leren,
         Y is de bekende uitvoer bij de testdata
         n is het getal waarmee X genormaliseerd wordt
@@ -12,9 +12,15 @@ class MultiLayerNeuralNetwork():
     def __init__(self, k, X, Y, n, m):
         self.l = np.shape(X)[-1] #lengte van 1 input
         self.j = np.size(Y) #aantal voorbeelden
-        w_len = k * (self.l + 1)
-        b_len = k + 1
+
+        b_len = sum(k) + 1
+        k = [self.l] + k
+        w_len = 0
+        for i in range(len(k)-1):
+            w_len += k[i] * k[i+1]
+        w_len += k[-1]    
         self.k = k
+        
         #p is een lijst met beginschattingen voor de gewichten [w1,w2,...wn,b]
         self.p = np.random.random(w_len + b_len)
         self.X = np.round(X/n, 1)
@@ -43,11 +49,16 @@ class MultiLayerNeuralNetwork():
 
     def bereken_y(self, invoer, p):
         s_uit = p[-1]
-        for eenheid in range(self.k):
-            wi = p[eenheid*self.l:(eenheid+1)*self.l] # gewichten
-            si = np.dot(invoer, wi) + p[-(self.k+1)+eenheid] # inp + bias
-            yt = self.sigma(si)
-            s_uit = s_uit + p[-(2*self.k+1)+eenheid] * yt # totaal + weging * y
+        off = 0 #plek in p waar de wegingen voor deze laag beginnen
+        offb = 0 #plek in p waar de biassen voor deze laag beginnen
+        for i in range(len(self.k)):            
+            for eenheid in range(self.k[i+1]):
+                wi = p[off+eenheid*self.l:off+(eenheid+1)*self.l] # gewichten
+                si = np.dot(invoer, wi) + p[-(sum(self.k)+ 1)+offb+eenheid] # inp + bias
+                yt = self.sigma(si)
+                s_uit = s_uit + p[-(2*self.k+1)+eenheid] * yt # totaal + weging * y
+            off += self.k[i] * self.k[i+1]
+            offb += k[i+1]
         return self.sigma(s_uit)
 
     def train(self, iteraties, alfa):
--- MultiLayer/nn multilayer begin.py
+++ MultiLayer/nn multilayer begin.py
@@ -12,8 +12,8 @@ X = np.array([[0,0,0], [0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0], [1,
 #Y = np.array([0, 1, 1, 1, 1, 1, 1, 1]) # minimaal 1
 Y = np.array([0, 0, 0, 1, 0, 1, 1, 1])# minimaal 2
 
-netwerk = MultiLayerNeuralNetwork(10, X, Y, 1, 1)
-netwerk.train(100, 1e0)
+netwerk = MultiLayerNeuralNetwork([10, 8], X, Y, 1, 1)
+#netwerk.train(100, 1e0)
 
  
 
