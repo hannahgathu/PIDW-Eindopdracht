87b7ca0edd16d6878da1bef0d848212da363bbea
raymondedwards95 <raymondedwards95@gmail.com>
2017-03-30 11:57:17 +0200

    update test mnist

--- test mnist data.py
+++ test mnist data.py
@@ -4,30 +4,14 @@ from TwoLayerNeuralNetwork import TwoLayerNeuralNetwork
 from NeuralNetwork import NeuralNetwork
 from read_ubyte import *
 
-
 Y = Y/9
 X = X/255
-"""
 
-Y = Y.astype('float16')
-print(type(Y[1]))
-"""
-
-'''
-# for row in X:
-#     print(n.predict(row))
-for t in range(100, 5001, 100):
-    y = Y[:t]
-    x = X[:t]
-    n = TwoLayerNeuralNetwork(5, x, y)
-    n.train(10, 1)
-    print("t = ", t, " Output:", n.predict(X[1]))
-'''
-
-m = NeuralNetwork(X, Y)
-m.train(1000, 0.01)
-i = 0
-for row in X:
-    uit = m.predict(row)
-    print("Uit:", uit*9, "Doel:", 9*Y[i])
-    i += 1
+n = TwoLayerNeuralNetwork(5, X, Y, 1, 1)
+n.train(1000, 0.01)
+uit = np.zeros(np.shape(X)[0])
+verschil = np.zeros(np.shape(X)[0])
+for i in range(np.shape(X)[0]):
+    uit[i] = n.predict(X[i])
+    verschil[i] = round(9 * uit[i]) - round(9 * Y[i])
+print(np.count_nonzero(verschil))
