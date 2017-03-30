11cc1924fef7f4f19dd91b14a2512ae774a9d83c
Raymond Edwards <raymondedwards95@gmail.com>
2017-03-30 21:17:55 +0200

    train data toegevoegd

--- TwoLayer/read_ubyte.py
+++ TwoLayer/read_ubyte.py
@@ -1,6 +1,6 @@
 import autograd.numpy as np
 
-def ReadLabels(file):
+def ReadLabels(file, aantal):
     """ open een ubyte-bestand, en vertaal de bytes naar labels
     Invoer: ubyte-bestand met labels
     Uitvoer: lijst met labels
@@ -8,17 +8,21 @@ def ReadLabels(file):
     f = open(file, 'rb')
     f.seek(8)
     y = np.fromfile(f, dtype=np.ubyte)
+    y = y[:aantal]
     f.close()
+    print("Labels  in '" + str(file) + "' gelezen.")
     return y
 
-def ReadImages(file):
+def ReadImages(file, aantal, h=28, b=28):
     """ open een ubyte-bestand met pixels, en vetaal de bytes
     Invoer: ubyte-bestand met afbeeldingen
-    Uitvoer: array met waarden 0-255 en dimensie (aantal, hoogte*breedte)
+    Uitvoer: array met waarden 0-255 en dimensies (aantal, hoogte*breedte)
     """
     f = open(file, 'rb')
     f.seek(16)
     p = np.fromfile(f, dtype=np.ubyte)
-    x = np.reshape(p, [10000, -1])
+    p = p[:aantal*h*b]
+    x = np.reshape(p, [aantal, -1])
     f.close()
+    print("Figuren in '" + str(file) + "' gelezen.")
     return x
