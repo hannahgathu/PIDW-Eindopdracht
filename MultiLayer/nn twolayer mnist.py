c6a26a02a469a3d0504628d3512d7ef6443ebfc5
Raymond Edwards <raymondedwards95@gmail.com>
2017-04-13 22:11:41 +0200

    weghalen van niet-nuttige regels

--- MultiLayer/nn twolayer mnist.py
+++ MultiLayer/nn twolayer mnist.py
@@ -12,26 +12,7 @@ TrainImages = ReadImages('train-images.idx3-ubyte', 60000)
 TestLabels = ReadLabels('t10k-labels.idx1-ubyte', 10000)
 TestImages = ReadImages('t10k-images.idx3-ubyte', 10000)
 
-n = MultiLayerNeuralNetwork(2, TrainImages, TrainLabels)
-
-print("Training...")
-n.train(50, 0.01)
-print("Klaar")
-
-print("Test traindata")
-uit = np.zeros(np.shape(TrainImages)[0])
-verschil = np.zeros(np.shape(TrainImages)[0])
-for i in range(np.shape(TrainImages)[0]):
-    uit[i] = n.predict(TrainImages[i])
-    verschil[i] = round(9 * uit[i]) - round(9 * TrainLabels[i])
-fout = np.count_nonzero(verschil)
-print("Er zijn {0} foute antwoorden gegeven. ({1:.1f}%)".format(fout, 100*fout/np.size(verschil)))
-
-print("\nTest testdata")
-uit = np.zeros(np.shape(TestImages)[0])
-verschil = np.zeros(np.shape(TestImages)[0])
-for i in range(np.shape(TestImages)[0]):
-    uit[i] = n.predict(TestImages[i])
-    verschil[i] = round(9 * uit[i]) - round(9 * TestLabels[i])
-fout = np.count_nonzero(verschil)
-print("Er zijn {0} foute antwoorden gegeven. ({1:.1f}%)".format(fout, 100*fout/np.size(verschil)))
+n = MultiLayerNeuralNetwork([50,30,10], TrainImages, TrainLabels)
+n.train(10, 0.01)
+n.bepaal_succes(TrainImages, TrainLabels)
+n.bepaal_succes(TestImages, TestLabels)
