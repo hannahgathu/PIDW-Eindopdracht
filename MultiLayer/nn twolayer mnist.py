23d25edf3bbd9b14ce40a2d634014b08b98469e7
Raymond Edwards <raymondedwards95@gmail.com>
2017-04-19 20:01:37 +0200

    bepaling goede parameters (?)
    de vooraf gedefinieerde 'factor' en 'alfa' werken bij dit specifieke
    geval (en een aantal andere). Er zit niet echt iets achter deze waarden,
    behalve dat er resultaten zijn te verkrijgen (25% fout bij het oefenen
    met de helft (30000 voorbeelden) van de testdata).

--- MultiLayer/nn twolayer mnist.py
+++ MultiLayer/nn twolayer mnist.py
@@ -12,9 +12,21 @@ TrainLabels = ReadLabels('train-labels.idx1-ubyte', 60000)
 TestImages = ReadImages('t10k-images.idx3-ubyte', 10000)
 TestLabels = ReadLabels('t10k-labels.idx1-ubyte', 10000)
 
-factor = 1/10000
-n = MultiLayerNeuralNetwork([5,5], TrainImages[:100], TrainLabels[:100], factor)
-n.train(1000, 0.001)
+# deze kunnen worden gevarieerd:
+k = [9,5]
+aantal = 5000
+
+# dit zijn werkende waarden,
+# bedoeld als hulpmiddel bij het vinden van goede parameters:
+factor = 1/(2 * sum(k))
+alfa = len(k) / (aantal * sum(k))
+
+n = MultiLayerNeuralNetwork(k, TrainImages[:aantal], TrainLabels[:aantal], factor)
+
+print("\nMaximale initiële weging is {}".format(factor))
+
+n.train(5000, alfa)
+
 n.bepaal_succes(TrainImages, TrainLabels)
 n.bepaal_succes(TestImages, TestLabels)
 
